bfb8172df75154b8b6ee30ec156b460e3e4de94e
Yumeng-zhang233 <77811013+Yumeng-zhang233@users.noreply.github.com>
2021-07-27 20:40:48 -0500

    first commit

---
+++ src/main/java/com/laioffer/tinnews/ui/home/HomeFragment.java
@@ -0,0 +1,52 @@
+package com.laioffer.tinnews.ui.home;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.laioffer.tinnews.R;
+import com.laioffer.tinnews.repository.NewsRepository;
+import com.laioffer.tinnews.repository.NewsViewModelFactory;
+
+public class HomeFragment extends Fragment {
+    private HomeViewModel viewModel;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        NewsRepository repository = new NewsRepository();
+        //Creates ViewModelProvider, which will create ViewModels via
+        // the given Factory and retain them in a store of the given ViewModelStoreOwner.
+        viewModel = new ViewModelProvider(this, new NewsViewModelFactory(repository)).get(HomeViewModel.class);
+        viewModel.setCountryInput("us");
+        viewModel
+                .getTopHeadlines()
+                .observe(
+                        getViewLifecycleOwner(),
+                        newsResponse -> {
+                            if (newsResponse != null) {
+                                Log.d("HomeFragment", newsResponse.toString());
+                            }
+                        });
+
+    }
+}
---
+++ src/main/java/com/laioffer/tinnews/ui/save/SaveFragment.java
@@ -0,0 +1,31 @@
+package com.laioffer.tinnews.ui.save;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.laioffer.tinnews.R;
+import com.laioffer.tinnews.repository.NewsRepository;
+import com.laioffer.tinnews.ui.home.HomeViewModel;
+
+public class SaveFragment extends Fragment {
+
+    public SaveFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_save, container, false);
+    }
+
+}
